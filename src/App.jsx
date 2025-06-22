748070c832b5494723bd7f3ce054b8db1be5b84f
ghofur <algar0940@gmail.com>
2025-06-22 20:49:30 +0700

    hapus project

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/skills" element={<Skills />}></Route>
-          <Route path="/projects" element={<Projects />}></Route>
+         {/* <Route path="/projects" element={<Projects />}></Route> */}
           <Route path="/contact" element={<Contact />}></Route>
         </Routes>
       </div>
